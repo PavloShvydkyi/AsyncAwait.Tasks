1bdc83486971e0b1f3f296ede07e4deac36eae9b
pshvydkyi-clgx <pshvydkyi@corelogic.com>
2025-05-06 13:14:45 +0300

    task_2

--- AsyncAwait.Task2.CodeReviewChallenge/Middleware/StatisticMiddleware.cs
+++ AsyncAwait.Task2.CodeReviewChallenge/Middleware/StatisticMiddleware.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using AsyncAwait.Task2.CodeReviewChallenge.Headers;
@@ -22,21 +23,30 @@ public class StatisticMiddleware
     public async Task InvokeAsync(HttpContext context)
     {
         string path = context.Request.Path;
-
-        var staticRegTask = Task.Run(
-            () => _statisticService.RegisterVisitAsync(path)
-                .ConfigureAwait(false)
-                .GetAwaiter().OnCompleted(UpdateHeaders));
-        Console.WriteLine(staticRegTask.Status); // just for debugging purposes
-
-        void UpdateHeaders()
-        {
-            context.Response.Headers.Add(
+        await _statisticService.RegisterVisitAsync(path);
+        long count = await _statisticService.GetVisitsCountAsync(path);
+        Console.WriteLine("Path {0}",path);
+        Console.WriteLine("Count {0}", count);
+        context.Response.Headers.Add(
                 CustomHttpHeaders.TotalPageVisits,
-                _statisticService.GetVisitsCountAsync(path).GetAwaiter().GetResult().ToString());
-        }
+                count.ToString());
+        /// Убрали использование Task.Run так так это не CPU bound task
+        /// sleep также убрали так как он не нужен 
 
-        Thread.Sleep(3000); // without this the statistic counter does not work
+        //var staticRegTask = Task.Run(
+        //    () => _statisticService.RegisterVisitAsync(path)
+        //        .ConfigureAwait(false)
+        //        .GetAwaiter().OnCompleted(UpdateHeaders));
+        //Console.WriteLine(staticRegTask.Status); // just for debugging purposes
+
+        //void UpdateHeaders()
+        //{
+        //    context.Response.Headers.Add(
+        //        CustomHttpHeaders.TotalPageVisits,
+        //        _statisticService.GetVisitsCountAsync(path).GetAwaiter().GetResult().ToString());
+        //}
+
+        //Thread.Sleep(3000); // without this the statistic counter does not work
         await _next(context);
     }
 }
--- AsyncAwait.Task2.CodeReviewChallenge/Services/PrivacyDataService.cs
+++ AsyncAwait.Task2.CodeReviewChallenge/Services/PrivacyDataService.cs
@@ -6,7 +6,10 @@ public class PrivacyDataService : IPrivacyDataService
 {
     public Task<string> GetPrivacyDataAsync()
     {
-        return new ValueTask<string>("This Policy describes how async/await processes your personal data," +
-                                     "but it may not address all possible data processing scenarios.").AsTask();
+        /// возвращаем сразу успешно завершенную таску без тайп кастинга 
+        return Task.FromResult("This Policy describes how async/await processes your personal data," +
+                                     "but it may not address all possible data processing scenarios.");
+        
+        //return new ValueTask<string>().AsTask();
     }
 }
